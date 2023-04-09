51cb04286f10b7fcef284e498dddc47112c83cc3
sidhant singh <singhsidmail23@gmail.com>
2023-04-09 17:11:19 +0530

    npm module bundling

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,57 @@
-import { useState, useEffect, useRef } from 'react'
-import * as esbuild from 'esbuild-wasm'
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import { useState, useEffect, useRef } from "react";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 function App() {
-  
-  const [input, setInput] = useState("")
-  const [code, setCode] = useState("")
-  const ref = useRef<any>()
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const ref = useRef<any>();
 
   const startService = async () => {
-     ref.current = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '../esbuild.wasm'
+      wasmURL: "../esbuild.wasm",
     });
-  }
+  };
 
   useEffect(() => {
-    startService()
-  }, [])
+    startService();
+  }, []);
 
   const onClick = async () => {
-    if(!ref.current){
+    if (!ref.current) {
       return;
     }
     const result = await ref.current.build({
-      entryPoints: ['index.js'],
+      entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()]
-    })
-    console.log(result)
+      plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+    console.log(result);
     setCode(result.outputFiles[0].text);
-  }
-
+  };
 
   return (
     <div className="App">
-      <textarea name="" id="" value={input} onChange={e => {setInput(e.target.value)}}></textarea>
+      <textarea
+        name=""
+        id=""
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      ></textarea>
       <div>
-        <button onClick={onClick}>
-          Submit
-        </button>
+        <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
