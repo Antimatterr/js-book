a2c2dc9fd20f3f7c0df3e56949e06dfa635d82d7
sidhant singh <singhsidmail23@gmail.com>
2023-04-09 19:15:46 +0530

    refactoring plugins and catching package files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 function App() {
   const [input, setInput] = useState('');
@@ -26,7 +27,7 @@ function App() {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(),fetchPlugin(input)], //** plugins are run from left to right. */
       define: {
         // 'process.env.NODE_ENV': '"production"',
         global: 'window',
